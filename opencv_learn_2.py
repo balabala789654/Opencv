1eac00e5309a5fc1cb91757f76015d911987f247
balabala789654 <2216953145@qq.com>
2024-06-13 13:23:56 +0800

    update

--- opencv_learn_2.py
+++ opencv_learn_2.py
@@ -1,14 +1,26 @@
 import cv2
 import matplotlib.pyplot as plt
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(1)
 
-while True:
+while cap.isOpened() and cap1.isOpened():
     
     ret, frame = cap.read()
-    cv2.imshow("img",frame)
+    ret1, frame1 = cap1.read()
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    print(frame.shape, "fps: ", cap.get(cv2.CAP_PROP_FPS))
+    if ret:
+        cv2.imshow("img",frame)
+    if ret1:
+        cv2.imshow("img1", frame1)
+        
     k = cv2.waitKey(1)
+
     if k == ord('q'):
         break
 
+cap.release()
+cap1.release()
 cv2.destroyAllWindows()
