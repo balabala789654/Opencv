3be836d8cdca514590b52f69c9893f16f9e5cc7f
balabala789654 <2216953145@qq.com>
2024-07-27 21:07:45 +0800

    update

--- learn/py_serial.py
+++ learn/py_serial.py
@@ -1,14 +1,26 @@
 import serial
 import serial.tools.list_ports
+import time
 
-ports_list = list(serial.tools.list_ports.comports())
-for comport in ports_list:
-    print(list(comport)[0], list(comport)[1])
-
-count = 100
-ser = serial.Serial(port='COM3', baudrate=115200, timeout=1) # 打开串口
-ser.write(f'the number is {count}') # 对串口发送数据
-data = ser.read(10) # 读取10个字节
-ser.close() # 关闭串口
+# write函数只支持传输bytes类型数据
+# 用b""表示bytes类型
+# 也可以使用encode()函数将字符串类型转换成bytes类型
+# 使用decode()函数将bytes类型转换成字符串类型
+if __name__ == "__main__":
+    ser = serial.Serial(port='COM10', 
+                        baudrate=115200, 
+                        bytesize=serial.EIGHTBITS, 
+                        parity=serial.PARITY_NONE, 
+                        stopbits=serial.STOPBITS_ONE, 
+                        timeout=1) # 打开串口
+    count = 1
+    while True:
+        result = ser.write(f"{count}".encode('utf-8')) # 对串口发送数据
+        # print("send data len: ", result)
+        count += 1
+        time.sleep(0.5)
+        
+    # data = ser.read(10) # 读取10个字节
+    # ser.close() # 关闭串口
 
 
--- learn/py_thread.py
+++ learn/py_thread.py
@@ -1,1 +1,33 @@
 import threading
+import time
+import queue
+
+# lock = threading.Lock()
+# shared_resource = 0
+
+my_queue = queue.Queue()
+
+def thread_entry(x):
+    for i in range(10000):
+        # time.sleep(1)
+        my_queue.put(f"entry: {i}") # 发送
+        print(f"entry_1 working times: {i}")
+
+def thread_entry_2(x):
+    while True:
+        msg = my_queue.get() # 接受
+        if msg is None:
+            break
+        print(msg)
+        my_queue.task_done() # 表明一个任务已完成
+
+thread1 = threading.Thread(target=thread_entry, args=("thread_1", ))
+thread2 = threading.Thread(target=thread_entry_2, args=("thread_2",))
+
+thread1.start()
+thread2.start()
+
+thread1.join() # 等待线程1完成
+my_queue.put(None) # 在线程1完成之后发送NONE表明不在发送数据
+thread2.join() # 等待线程2完成
+print("all task done")
